d4d843277c48b50910224bf16875af08baa18950
tt <496181660@qq.com>
2025-12-26 17:42:09 +0800

    fix(routes): 修正数字货币支付回调方法名
    - 将 digitalNotify 路由的控制器方法从 digitalNotify 改为 sfPayDigitalNotify
    - 保持中间件和路由名称不变

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::prefix('orders')->group(function () {
     Route::get('detail/{order_id}', [OrderController::class, 'detail'])->name('订单详情');
     Route::any('payNotify', [OrderController::class, 'payNotify'])->middleware('redis.lock:payNotify:{orderNo},15')->name('etPay支付回调');
     Route::post('sfPayNotify', [OrderController::class, 'sfPayNotify'])->middleware('redis.lock:sfPayNotify:{merchant_order_sn},15')->name('sfPay法币支付回调');
-    Route::post('digitalNotify', [OrderController::class, 'digitalNotify'])->middleware('redis.lock:digitalNotify:{merchant_order_sn},15')->name('sfPay数字货币支付回调');
+    Route::post('digitalNotify', [OrderController::class, 'sfPayDigitalNotify'])->middleware('redis.lock:digitalNotify:{merchant_order_sn},15')->name('sfPay数字货币支付回调');
 });
 // 文案
 Route::prefix('set')->group(function () {
